d5f4cad56da4d655bc4d843248d9f4e67351379c
mshivam-kumar <svmkumar28@gmail.com>
2025-11-23 20:02:36 +0530

    Fix: Update logo path for production

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,7 +1,7 @@
 const appConfig = {
   companyName: 'Torvyn AI Private Limited',
   productName: 'Torvyn Financial Close Automation (FCA) Agentic Platfrom',
-  logoPath: '/src/assets/logo.png',
+  logoPath: '/assets/logo-DPCrL5KK.png',
   contactEmail: 'torvynai@gmail.com',
   websiteUrl: 'https://torvynai.com'
 } as const
